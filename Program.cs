e8e290d99e9990b32223e0a53a958688b4d52e0b
Jacob-Carrell-Student <jacaa29@student.oaisd.org>
2024-10-07 13:03:04 +0000

    Wrote some C# code, this is pretty cool!

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Hello World!");
+        Console.WriteLine ("My name is Jacob Cameron Carrell.");
+
+        int age = 17;
+        string greeting = "Hello World";
+        string firstname = "Jacob";
+        string middlename = "Cameron";
+        string lastname = "Carrell";
+        double money = 4.21;
+
+        Console.WriteLine(greeting + "! " + "My name is " + firstname + " " + middlename + " " + lastname + ". I am " + age + " years old. I just spent $" + money + " on some Little Caesars Crazy Puffs.");
+    }
+}
